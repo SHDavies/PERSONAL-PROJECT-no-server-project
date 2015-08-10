281d075913a73ca54a3c18adda95601f7a154c8f
Spencer Davies <spencerhdavies@gmail.com>
2015-08-10 11:51:07 -0600

    Update billService.js

--- js/billService.js
+++ js/billService.js
@@ -3,7 +3,7 @@ app.service('billService', function($http, $q) {
 	//grab sponsored bills for a given bioguide_id
 	this.billSearch = function(repId) {
 		var deferred = $q.defer();
-		$http.get('https://congress.api.sunlightfoundation.com/bills?sponsor_id=' + repId + '&order=last_action_at&apikey=dac579d765324189876e07b53a5815ce')
+		$http.get('https://congress.api.sunlightfoundation.com/bills?sponsor_id=' + repId + '&order=last_action_at&apikey=xxxxxxx')
 		.then(function(response) {
 			var data = response.data.results;
 			deferred.resolve(data)
